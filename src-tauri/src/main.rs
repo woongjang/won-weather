12050fef0b77e4b2c00a411bf10bc25a37da4496
WON <won.develop@gmail.com>
2023-01-30 22:22:59 +0900

    add features on tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -12,7 +12,62 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let open = CustomMenuItem::new("open".to_string(), "Open");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
+    let system_tray_menu = SystemTrayMenu::new().add_item(open).add_item(quit);
     tauri::Builder::default()
+        // .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            // SystemTrayEvent::LeftClick {
+            //     position: _,
+            //     size: _,
+            //     ..
+            // } => {
+            //     let window = app.get_window("main").unwrap();
+            //     if window.is_visible().unwrap() {
+            //         window.hide().unwrap();
+            //     } else {
+            //         window.show().unwrap();
+            //         window.set_focus().unwrap();
+            //     }
+                
+            // }
+            SystemTrayEvent::RightClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a right click");
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a double click");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "open" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(is_focused) => {
+                if !is_focused {
+                    event.window().hide().unwrap();
+                }
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
